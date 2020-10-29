bee4ca45d35f6feb54c861014ae6da64952c6c2c
Luaa <54554264+TheOfficialDevJosh@users.noreply.github.com>
2020-10-29 08:55:47 -0400

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -17,30 +17,6 @@ function Login() {
                 <div className="star"></div>
             </div>
 
-
-            <div className="loader">
-                <span style="--i:1;"></span>
-                <span style="--i:2;"></span>
-                <span style="--i:3;"></span>
-                <span style="--i:4;"></span>
-                <span style="--i:5;"></span>
-                <span style="--i:6;"></span>
-                <span style="--i:7;"></span>
-                <span style="--i:8;"></span>
-                <span style="--i:9;"></span>
-                <span style="--i:10;"></span>
-                <span style="--i:11;"></span>
-                <span style="--i:12;"></span>
-                <span style="--i:13;"></span>
-                <span style="--i:14;"></span>
-                <span style="--i:15;"></span>
-                <span style="--i:16;"></span>
-                <span style="--i:17;"></span>
-                <span style="--i:18;"></span>
-                <span style="--i:19;"></span>
-                <span style="--i:20;"></span>
-            </div>
-
             <h2>You need to login in order to enter the app!</h2>
 
             <div className="login__logo">
